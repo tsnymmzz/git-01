6730572b653b0993edff46bd38e512e4d11363a5
wang <2549408353@qq.com>
2024-07-23 12:59:39 +0800

    tomorrow commit

--- src/main/java/wang/GitTest.java
+++ src/main/java/wang/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("git test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("pull test 2");
     }
 }
