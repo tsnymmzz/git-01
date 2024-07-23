452ca2d6b0026a51353c432be84421344ca7dd90
tsnymmzz <130818772+tsnymmzz@users.noreply.github.com>
2024-07-23 13:04:54 +0800

    Update web

--- src/main/java/wang/GitTest.java
+++ src/main/java/wang/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("pull test");
         System.out.println("pull test 2");
+        System.out.println("push test 2");
     }
 }
